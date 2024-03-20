ac4f9094813262ee2e7886cd0878529a65b45dc6
root <root@Desktop-TJAS>
2024-03-19 23:28:08 -0300

    implement mfcc

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,20 +1,35 @@
 import numpy as np
 from scipy.fft import fft
+import librosa
 
 class FeatureExtractor:
     def __init__(self):
         pass
 
     def extract_features(self, frames, metadata):
-        audios = []
+        # audios = []
 
-        for index, row in metadata.iterrows():
-            class_id = row['Class ID']
-            audio_data = frames[index]
-            transformada = fft(audio_data)
-            audios.append((np.abs(transformada)[:20000], class_id))
+        # for index, row in metadata.iterrows():
+        #     class_id = row['Class ID']
+        #     audio_data = frames[index]
+        #     transformada = fft(audio_data)
+        #     audios.append((np.abs(transformada)[:20000], class_id))
 
-        x = np.array([audio[0] for audio in audios])
-        y = np.array([audio[1] for audio in audios])
+        # x = np.array([audio[0] for audio in audios])
+        # y = np.array([audio[1] for audio in audios])
 
+        # return x, y
+
+        x = []
+        y = []
+        
+        for frame in frames:
+            audio_data, class_id = frame
+            mfcc_features = librosa.feature.mfcc(y=audio_data, sr=22050)
+            mfcc_features = mfcc_features.flatten()
+
+            x.append(mfcc_features)
+
+            y.append(class_id)
+            
         return x, y
--- preprocessing.py
+++ preprocessing.py
@@ -13,10 +13,10 @@ class Preprocess:
     def load_metadata(self):
         self.data = pd.read_csv(self.metadata_file)
 
-    def create_frames(self, audio_file):
+    def create_frames(self, audio_file, class_id):
         audio_data, sample_rate = librosa.load(os.path.join(self.audio_dir, audio_file), sr=22050)
         frame_samples = int(sample_rate * self.frame_length)
-        frames = [audio_data[i:i+frame_samples] for i in range(0, len(audio_data), frame_samples)]
+        frames = [(audio_data[i:i+frame_samples],class_id) for i in range(0, len(audio_data), frame_samples)]
         return frames
 
     def process_audio_files(self):
@@ -25,7 +25,8 @@ class Preprocess:
 
         for index, row in self.data.iterrows():
             dataset_file = row['Dataset File Name']
-            frames = self.create_frames(dataset_file)
+            class_id = row['Class ID']
+            frames = self.create_frames(dataset_file, class_id)
             self.frames.extend(frames)
 
         print("Preprocessing completed. Frames created.")
